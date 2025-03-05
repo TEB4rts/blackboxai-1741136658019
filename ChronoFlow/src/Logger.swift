4c597f3a8d5cd8edc1030d951570f3c9170b5604
TEB4rts <65622113+TEB4rts@users.noreply.github.com>
2025-03-05 09:13:55 +0800

    Add ChronoFlow/src/Logger.swift 1741137233826

--- ChronoFlow/src/Logger.swift
+++ ChronoFlow/src/Logger.swift
@@ -1,5 +1,27 @@
 import Foundation
+import os.log
 
 class Logger {
-    // Logging functionality will go here
+    static let shared = Logger()
+    private let log: OSLog
+
+    private init() {
+        log = OSLog(subsystem: "com.chronoflow", category: "general")
+    }
+
+    func log(_ message: String, level: OSLogType = .default) {
+        os_log("%{public}@", log: log, type: level, message)
+    }
+
+    func logError(_ message: String) {
+        log(message, level: .error)
+    }
+
+    func logInfo(_ message: String) {
+        log(message, level: .info)
+    }
+
+    func logDebug(_ message: String) {
+        log(message, level: .debug)
+    }
 }
