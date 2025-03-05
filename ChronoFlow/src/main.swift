0ece4e8940bc5550be2dc38dc1f0049130867c3c
TEB4rts <65622113+TEB4rts@users.noreply.github.com>
2025-03-05 09:04:36 +0800

    Add ChronoFlow/src/main.swift 1741136674704

---
+++ ChronoFlow/src/main.swift
@@ -0,0 +1,11 @@
+// Main entry point for the ChronoFlow app
+import SwiftUI
+
+@main
+struct ChronoFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
